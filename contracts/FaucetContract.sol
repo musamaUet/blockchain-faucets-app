05c48c39762168407222f0a427a9b8eadb565d14
musamauet <usamarabani@renesistech.com>
2024-01-14 21:36:40 +0500

    update FaucetContracts

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -1,14 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "./Owned.sol";
+import "./Logger.sol";
+import "./IFaucet.sol";
 
-contract Faucet {
+contract Faucet is Owned, Logger, IFaucet {
  
     uint public numOfFunders;
+
     mapping(address => bool) private funders;
     mapping(uint => address) private lutFunders;
     receive() external payable {}
 
-    function addFunds() external payable {
+    function emitLog() public override pure returns(bytes32) {
+        return "Hello World";
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        owner = newOwner;
+    }
+
+    modifier limitWithdraw(uint withdrawAmount) {
+        require(withdrawAmount <= 1000000000000000000, "Cannot withdraw more than 1 ether");
+        // This underscore means that if above exception will not occur, then function body will be executed.
+        _;
+    }
+
+    function addFunds() override external payable {
         // Msg is a special object that holds some information related to the transaction.
         address funder = msg.sender;
         if(!funders[funder]) {
@@ -29,6 +47,23 @@ contract Faucet {
         return _funders;
     }
 
+    function test1() external onlyOwner {
+        // some managing stuff that only admin should have access to. 
+    }
+
+    function test2() external onlyOwner {
+        // some managing stuff that only admin should have access to.
+    }
+
+    function withdraw(uint withdrawAmount) external limitWithdraw(withdrawAmount) {
+        // if our amount is less than 1 ETH
+        // if (withdrawAmount < 1000000000000000000) {
+        //     payable(msg.sender).transfer(withdrawAmount);
+        // }
+        // require(withdrawAmount <= 1000000000000000000, "Cannot withdraw more than 1 ether");
+        payable(msg.sender).transfer(withdrawAmount);
+    }
+
     function getFunderAtIndex(uint8 index) external view returns(address) {
         return lutFunders[index];
     }
@@ -43,4 +78,6 @@ contract Faucet {
 // instance.addFunds({from: accounts[0], value:"200000000"});
 // instance.getFunderAtIndex(0);
 // instance.getAllFunders();
+
+// instance.withdraw("500000000000000000", {from: accounts[0]})
 

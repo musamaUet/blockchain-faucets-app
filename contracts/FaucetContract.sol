6850e0021c36a06368ed2ad7b91aedc39d753f67
musamauet <usamarabani@renesistech.com>
2024-01-14 14:42:18 +0500

    update storage

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,24 +3,44 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
  
-    address[] public funders;
+    uint public numOfFunders;
+    mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
     receive() external payable {}
 
     function addFunds() external payable {
         // Msg is a special object that holds some information related to the transaction.
-        funders.push(msg.sender);
+        address funder = msg.sender;
+        if(!funders[funder]) {
+            uint index = numOfFunders++;
+            funders[funder] = true;
+            lutFunders[index] = funder;
+        }
+
     }
 
-    function getAllFunders() public view returns(address[] memory) {
-        return funders;
+    function getAllFunders() external view returns(address[] memory) {
+        address[] memory _funders = new address[](numOfFunders);
+
+        for(uint i = 0; i< numOfFunders; i++) {
+            _funders[i] = lutFunders[i];
+        }
+
+        return _funders;
     }
 
     function getFunderAtIndex(uint8 index) external view returns(address) {
-        address[] memory _funders = getAllFunders();
-        return _funders[index];
+        return lutFunders[index];
     }
 }
 
 // Block Info
 // Nonce - a hash that when combined with the minHash proofs that the block has gone through proof of work(POW).
 // 8 Bytes => 64 Bits
+
+
+// const instance = await Faucet.deployed();
+// instance.addFunds({from: accounts[0], value:"200000000"});
+// instance.getFunderAtIndex(0);
+// instance.getAllFunders();
+
--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -2,6 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Storage {
+    mapping(uint => uint) public aa;
+    mapping(address => uint) public bb;
+    uint[] public cc;
     uint8 public a = 7;     // 1 Byte
     uint16 public b = 10;   // 1 Byte
     address public c = 0x4E702f6FEC4E9a8c2cdb730346fb96dF8fce1C31;  // 20 Byte
@@ -10,4 +13,12 @@ contract Storage {
     uint256 public f = 200; // 32 Byte
     uint8 public g = 40;    // 1 Byte
     uint256 public h = 789; // 32 Byte
+
+    constructor() {
+        aa[2] = 4;
+        aa[3] = 10;
+
+        bb[0xcAe1a86c6DA0ED70d45Ca25F4195F5f31Cbae76e] = 100;
+
+    }
 }

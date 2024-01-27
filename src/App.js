fae6fa7b2d98c317b94bb443595ecbf4827ac2ba
musamauet <usamarabani@renesistech.com>
2024-01-27 13:44:07 +0500

    implement web3 Detection and connection implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,57 @@
 import React from 'react';
 
 function App() {
+
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  });
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      // With metamask, we have an access to window.ethereum & to window.web3
+      // metamask injects a global API into website.
+      //  this api allows websites to request users, accounts, read data to blockchain,
+      // sign messages and transactions
+
+      const provider = await detectEthereumProvider();
+      if(provider) {
+        // provider.request({method:"eth_requestAccounts"});
+        setWeb3Api({web3: new Web3(provider), provider});
+      } else {
+        console.error("Install metamask.");  
+      }
+
+      // if (window.ethereum) {
+      //   provider = window.ethereum;
+      //   try {
+      //     // await provider.enable();
+      //     await provider.request({method: "eth_requestAccounts"});
+      //   } catch(err) {
+      //     console.log("User denied account access!");
+      //   }
+      // } else if (window.web3) {
+      //   provider = window.web3;
+      // } else if (!process.env.production) {
+      //   provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      // }
+    }
+
+    loadProvider();
+  }, [])
+
+  useEffect(() => {
+    const getAccounts = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    }
+
+    web3Api.web3 && getAccounts();
+  }, [web3Api.web3])
+
+  console.log('web3Api', web3Api);
+
   return (
     <div>
       Faucet web app
